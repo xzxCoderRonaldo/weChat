1a3614b4a2d39b05c44f1e157bd7cd092c1b03c3
mac <mac@macs-macbook-air.local>
2016-06-15 12:53:44 +0800

    chushihua

---
+++ wexin/wexin/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  wexin
+//
+//  Created by mac on 16/6/15.
+//  Copyright © 2016年 ronaldo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    
+}
+
+@end
