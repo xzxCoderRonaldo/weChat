f1a4010f4397568b8af80875af6058c5222edcf9
mac <mac@macs-macbook-air.local>
2016-06-15 12:55:42 +0800

    button

---
+++ wexin/wexin/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  wexin
+//
+//  Created by mac on 16/6/15.
+//  Copyright © 2016年 ronaldo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ wexin/wexin/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  wexin
+//
+//  Created by mac on 16/6/15.
+//  Copyright © 2016年 ronaldo. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
